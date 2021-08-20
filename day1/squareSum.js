d9c3b10df1af399aba2ee6dc54388f5e721809ce
Josh Disney <joshua.a.disney@gmail.com>
2021-08-20 03:44:19 +0000

    Initial commit

---
+++ day1/squareSum.js
@@ -0,0 +1,22 @@
+/**
+ * @name Square(n) Sum
+ *
+ * @description
+ *  Complete the square sum function so that it squares each number
+ *  passed into it and then sums the results together.
+ *  For example, for [1, 2, 2] it should return 9 because 
+ *  1^2 + 2^2 + 2^2 = 9.
+ * 
+ * @param {number[]} numbers
+ * 
+ */
+
+function squareSum(numbers) {
+let total = 0
+numbers.forEach(i => {
+  total += i**2
+})
+return total
+}
+
+module.export = { squareSum };
